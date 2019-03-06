91e1fab9b355a0ce81694e0678d31bc0b6eb6204
david potter <davidjohnpotter@hotmail.com>
2019-03-05 18:52:31 -0700

    Add project files.

---
+++ ATM02/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ATM02.Startup))]
+namespace ATM02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
